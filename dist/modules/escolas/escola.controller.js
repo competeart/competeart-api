80abf570ccdb4fc759125c729714c35a07923a57
Saulo <saulorodm@gmail.com>
2026-04-28 16:15:34 -0300

    chore: atuliza gitignore

--- dist/modules/escolas/escola.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.criarEscolaController = criarEscolaController;
-const escola_service_1 = require("./escola.service");
-async function criarEscolaController(app, request, reply) {
-    const service = new escola_service_1.EscolaService(app.prisma);
-    const escola = await service.criar(request.body);
-    reply.code(201).send({ id: escola.id });
-}
--- dist/modules/independentes/independente.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.criarIndependenteController = criarIndependenteController;
-const independente_service_1 = require("./independente.service");
-async function criarIndependenteController(app, request, reply) {
-    const service = new independente_service_1.IndependenteService(app.prisma);
-    const inscricao = await service.criar(request.body);
-    reply.code(201).send({ id: inscricao.id });
-}
